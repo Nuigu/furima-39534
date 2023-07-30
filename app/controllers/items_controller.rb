ae3fd1248655d4a62509ee0ef8c24d73ca57c2cd
Nuigu <nuiguruzaku@gmail.com>
2023-07-30 09:59:15 +0900

    indexアクションにインスタンス変数@itemsを定義・一覧ページに表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
   before_action :move_to_signin, only: [:new, :create]
 
   def index
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
   def new
