97b4a5897b88b4e710e4929a00c55a1672733c1e
Nuigu <nuiguruzaku@gmail.com>
2023-08-16 12:22:40 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "3.17.3"
 set :application, 'furima-39534'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@https://github.com/Nuigu/furima-39534.git'
+set :repo_url,  'git@github.com:Nuigu/furima-39534.git'
 set :branch, 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
